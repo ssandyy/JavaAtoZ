86e81c5c171fd225d0168d947ea55cca6e0990e5
sandeep <ssandyy789@gmail.com>
2023-08-02 23:28:49 +0530

    NameAsTrianglePattern

---
+++ pattern/NameAsTrianglePattern.java
@@ -0,0 +1,18 @@
+public class NameAsTrianglePattern {
+    public static void main(String... args) {
+        String name = "SANDEEP_KUMAR_YADAV";
+        int k = 0;
+
+        for (int i = 0; i <= (name.length()) && k < name.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                // for (int j = 0; j <= i && k < name.length(); j++) {
+                if (k < name.length()) {
+                    System.out.print(name.charAt(k));
+                    k++;
+                }
+            }
+            System.out.println();
+            // }
+        }
+    }
+}
