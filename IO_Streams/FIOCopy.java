83658512a108fe3b75896198c4927015634bc928
ssandyy <ssandyy789@gmail.com>
2023-10-04 12:57:22 +0530

    FIOCopy

---
+++ IO_Streams/FIOCopy.java
@@ -0,0 +1,19 @@
+//package IO_Streams;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class FIOCopy {
+    public static void main(String[] args) throws IOException, FileNotFoundException {
+        FileInputStream fis = new FileInputStream("abc2.txt");
+        FileOutputStream fos = new FileOutputStream("bcd.txt");
+
+        int data;
+        while((data=fis.read())!=-1){
+            fos.write(data);
+        }
+        System.out.println("data copied successfully..!");
+    }
+}
