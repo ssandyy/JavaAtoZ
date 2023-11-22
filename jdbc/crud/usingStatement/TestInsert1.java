c5262f31e5c113b070035d56a01f1d17c8e351ce
ssandyy <ssandyy789@gmail.com>
2023-11-23 00:15:01 +0530

    old

--- jdbc/crud/usingStatement/TestInsert1.java
+++ jdbc/crud/usingStatement/TestInsert1.java
@@ -1,18 +1,24 @@
-
 import java.sql.*;
 
 public class TestInsert1 {
     public static void main(String[] args) {
         try {
-            Class.forName("oracle.jdbc.OracleDriver");
-//            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+           Class.forName("oracle.jdbc.driver.OracleDriver");
+        //    DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
             Connection c = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system", "oracle");
 //            Connection c = DriverManager.getConnection("jdbc:mysql://localhost/", "root", "");
+
+            System.out.println("Connected to database");
+
             int id = 4;
             String name = "rahul";
             int age = 55;
             Statement stm = c.createStatement();
-            int x = stm.executeUpdate("insert into emp values(" + id + ",'" + name + "','" + age + "')");
+
+            // int z = stm.executeUpdate("Create table emp(id number, name varchar2(100), age number)");
+            // System.out.println(z);
+
+            int x = stm.executeUpdate("insert into emp values(" + id + ",'" + name + "'," + age + ")");
             System.out.println(x);
             ResultSet rs = stm.executeQuery("select * from emp");
             while (rs.next()) {
@@ -26,6 +32,5 @@ public class TestInsert1 {
 		catch (Exception e) {
             System.out.println("Error occur while running program: " + e);
         }
-
     }
 }
